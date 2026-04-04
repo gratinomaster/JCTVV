2dbb4c2229ad5f93822ee2eef41ef3eb9b4d4063
gratinomaster <contato@guilhermemartins.tv>
2026-04-04 00:37:08 -0300

    Delete check_channels.sh

--- check_channels.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Extract all channel IDs from EPG
-curl -s "https://raw.githubusercontent.com/globetvapp/epg/main/Venezuela/venezuela1.xml" | grep -oP 'channel id="[^"]*"' | sed 's/channel id="//;s/"$//' | sort -u > /tmp/epg_channels.txt
-echo "Total unique channels in EPG:"
-wc -l < /tmp/epg_channels.txt
